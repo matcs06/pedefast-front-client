428f4610c44ce657c8983a9055123acc82083876
“matcs06” <you@example.com>
2023-07-05 10:55:16 -0300

    fixing page redirect

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 
 module.exports = {
   images: {
-    domains: ['www.acasaencantada.com.br', 'localhost'],
+    domains: ['www.acasaencantada.com.br', 'localhost', 'api.pedefast.com'],
 
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import instace from '@/api/appAPI'
 
 const inter = Inter({ subsets: ['latin'] })
 
+interface IUser {
+  username: string;
+  business_name: string;
+}
+
 export default function Home() {
+  const [admUsers, setAdmUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+
+    async function loadUsers() {
+      const response = await instace.get("/users")
+      setAdmUsers(response.data)
+    }
+
+    loadUsers()
+
+    return () => {
+      setAdmUsers([])
+    }
+
+  }, [])
+
+  function redirectToUserPage(username: string) {
+    window.location.pathname = (`/StartPage/${username}`)
+  }
+
   return (
     <>
       <Head>
@@ -15,108 +43,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <p>Bem vindo ao pedefast, escolha sua loja e veja seus produtos e servicos</p>
+        {admUsers.map((user) => (
+          <div className='w-2/3 bg-secondary-orange h-14 p-2 rounded-md cursor-pointer' onClick={() => { redirectToUserPage(user.username) }}>
+            <p className='text-light-gray'>{user.business_name}</p>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        ))}
       </main>
     </>
   )
